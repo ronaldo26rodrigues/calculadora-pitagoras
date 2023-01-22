d8988634ec481e1b3029ad2539b37144296ab51c
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2023-01-22 19:19:34 -0300

    documentação triângulo

--- calculadora-pitagoras/src/App.tsx
+++ calculadora-pitagoras/src/App.tsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import './components/Canvas'
-import Canvas from './components/Canvas'
-import { Triangulo } from './components/Triangulo'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "./components/Canvas";
+import Canvas from "./components/Canvas";
+import { Triangulo } from "./components/Triangulo";
 
 function App() {
-
   const canvasSize = 500;
   const canvasPadding = 60;
 
-  const [count, setCount] = useState(3)
+  const [count, setCount] = useState(3);
 
-  var [valoresAbsolutos, setValoresAbsolutos] = useState(
-    {
-      "catetoB": 3,
-      "catetoA": 4,
-      "hipotenusa": 5,
-      "anguloB": 36.87,
-      "anguloA": 53.13,
-      "altura": 2.4,
-      "h1": 3,
-      "h2": 2
-    }
-  )
+  var [valoresAbsolutos, setValoresAbsolutos] = useState({
+    catetoB: 3,
+    catetoA: 4,
+    hipotenusa: 5,
+    anguloB: 36.87,
+    anguloA: 53.13,
+    altura: 2.4,
+    h1: 3,
+    h2: 2,
+  });
 
-  
-  
   return (
     <div className="App">
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        botao
+      </button>
 
-      <button onClick={()=>{
-        setCount(count+1)
-        
-      }}>botao</button>
-      
-      <Triangulo canvasSize={500} canvasPadding={60} valoresAbsolutos={valoresAbsolutos} />
-      
+      <Triangulo
+        canvasSize={500}
+        canvasPadding={60}
+        valoresAbsolutos={valoresAbsolutos}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- calculadora-pitagoras/src/components/Triangulo.tsx
+++ calculadora-pitagoras/src/components/Triangulo.tsx
@@ -2,28 +2,38 @@ import React from "react";
 import Canvas from "./Canvas";
 
 interface TrianguloProps {
-    canvasSize: number,
-    canvasPadding: number
-    valoresAbsolutos: {"catetoA": number, "catetoB": number, "anguloA": number, "anguloB": number, "hipotenusa": number, "altura": number, "h1": number, "h2": number},
-    
-
+  canvasSize: number;
+  canvasPadding: number;
+  valoresAbsolutos: {
+    catetoA: number;
+    catetoB: number;
+    anguloA: number;
+    anguloB: number;
+    hipotenusa: number;
+    altura: number;
+    h1: number;
+    h2: number;
+  };
 }
 
 export function Triangulo({canvasSize, valoresAbsolutos, canvasPadding}:TrianguloProps) {
     var valoresRelativos = {
-        "catetoA": 0,
-        "catetoB": 0,
-        "hipotenusa": 0,
-        "anguloB": 0,
-        "anguloA": 0,
-        "altura": 0,
-        "h1": 0,
-        "h2": 0
-      }
-    var proporcaoHipotenusa = 0;
+      catetoA: 0,
+      catetoB: 0,
+      hipotenusa: 0,
+      anguloB: 0,
+      anguloA: 0,
+      altura: 0,
+      h1: 0,
+      h2: 0,
+    }; // estes valores são calculados para que o triângulo se encaixe na área do canvas
+
+    var proporcaoHipotenusa = 0; // valor da reta m, cateto de um dos triângulos formados pela reta da altura
     
-    function calculoMedidas() {
-        const proporcao = ((canvasSize-canvasPadding)/(valoresAbsolutos["catetoA"]>valoresAbsolutos["catetoB"]?valoresAbsolutos["catetoA"]:valoresAbsolutos["catetoB"]));
+    // calcula as medidas relativas ao canvas, partindo dos valores vindos da calculadora
+    function calculoMedidasRelativas() {
+        // o maior cateto encaixa no tamanho máximo do canvas, e os demais valores são calculados a partir desta proporção
+        const proporcao = ((canvasSize-canvasPadding)/(valoresAbsolutos["catetoA"]>valoresAbsolutos["catetoB"]?valoresAbsolutos["catetoA"]:valoresAbsolutos["catetoB"])); 
         valoresRelativos["catetoA"] = valoresAbsolutos['catetoA']*proporcao;
         valoresRelativos["catetoB"] = valoresAbsolutos['catetoB']*proporcao;
         valoresRelativos["hipotenusa"] = valoresAbsolutos['hipotenusa']*proporcao;
@@ -32,17 +42,17 @@ export function Triangulo({canvasSize, valoresAbsolutos, canvasPadding}:Triangul
         valoresRelativos["h2"] = valoresAbsolutos['h2']*proporcao;
         
         proporcaoHipotenusa = (valoresRelativos['h1']*100)/valoresRelativos['hipotenusa']/100;
-        console.log(proporcaoHipotenusa);
-        
     }
-    calculoMedidas()
+    calculoMedidasRelativas()
+    // calcula o ponto de partida do desenho, para que o triângulo fique centralizado
     var pontoPartida = {"x": (canvasSize/2)-valoresRelativos["catetoB"]/2, "y": (canvasSize/2)-valoresRelativos["catetoA"]/2}
 
     return <Canvas width={canvasSize} height={canvasSize} draw={(canvas)=>{
+
         const ctx = canvas.getContext('2d');
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
-        var grd = ctx.createLinearGradient(100, 100, canvas.width,canvas.height);
+        var grd = ctx.createLinearGradient(100, 100, canvas.width,canvas.height); // cor de fundo do canvas
         grd.addColorStop(0, "#F29B30");
         grd.addColorStop(1, "#BFB304");
 
@@ -54,26 +64,30 @@ export function Triangulo({canvasSize, valoresAbsolutos, canvasPadding}:Triangul
         // Fill with gradient
         ctx.fillStyle = grd;
         ctx.fillRect(0,0,canvas.width,canvas.height);
-        ctx.beginPath();
-        ctx.moveTo(pontoPartida["x"],pontoPartida["y"]); // angulo cateto oposto
-        ctx.lineTo(pontoPartida["x"],pontoPartida['y']+valoresRelativos['catetoA']); // cateto oposto: 170px
-        ctx.moveTo(pontoPartida["x"],pontoPartida['y']+valoresRelativos['catetoA']); // angulo reto
-        ctx.lineTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']); // cateto adjascente: 270px
-        ctx.moveTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']); // angulo cateto adjascente
-        ctx.lineTo(pontoPartida["x"],pontoPartida["y"]); // hipotenusa: 320
 
-        // altura: 275,2
-        // centro (150,100)
+        ctx.beginPath(); 
+        ctx.moveTo(pontoPartida["x"],pontoPartida["y"]); // posiciona o cursor no ponto A
+        ctx.lineTo(pontoPartida["x"],pontoPartida['y']+valoresRelativos['catetoA']); // desenha o cateto a
+        ctx.moveTo(pontoPartida["x"],pontoPartida['y']+valoresRelativos['catetoA']); // posiciona o cursor no ponto do angulo reto
+        ctx.lineTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']); // desenha o cateto b
+        ctx.moveTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']); // posiciona o cursor no ponto B
+        ctx.lineTo(pontoPartida["x"],pontoPartida["y"]); // desenha a hipotenusa
         
-        ctx.moveTo(pontoPartida['x'], pontoPartida['y']+valoresRelativos['catetoA'],200);
-        ctx.lineTo(pontoPartida['x']+(proporcaoHipotenusa*valoresRelativos['catetoB']), pontoPartida['y']+(proporcaoHipotenusa)*valoresRelativos['catetoA']);
-
+        ctx.moveTo(pontoPartida['x'], pontoPartida['y']+valoresRelativos['catetoA']); // posiciona o cursor no ponto do angulo reto
+        ctx.lineTo(pontoPartida['x']+(proporcaoHipotenusa*valoresRelativos['catetoB']), pontoPartida['y']+(proporcaoHipotenusa)*valoresRelativos['catetoA']); // desenha a reta h (altura)
 
         ctx.stroke()
-        ctx.moveTo(pontoPartida['x'],pontoPartida['y']);
-        ctx.arc(pontoPartida['x'],pontoPartida['y'], 40, (Math.PI/180)*90, (Math.PI/180)*valoresAbsolutos['anguloA'], true);
-        ctx.moveTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']);
-        ctx.arc(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA'],40,(Math.PI/180)*180, (Math.PI/180)*(180+valoresAbsolutos['anguloA']), false);
+
+        ctx.moveTo(pontoPartida['x'],pontoPartida['y']); // posiciona o cursor no ponto A
+        ctx.arc(pontoPartida['x'],pontoPartida['y'], 40, (Math.PI/180)*90, (Math.PI/180)*valoresAbsolutos['anguloA'], true); // desenha o ângulo do ponto A
+        ctx.moveTo(pontoPartida['x']+valoresRelativos['catetoB'],pontoPartida['y']+valoresRelativos['catetoA']); // posiciona o cursor no ponto B
+        ctx.arc(
+            pontoPartida['x']+valoresRelativos['catetoB'],
+            pontoPartida['y']+valoresRelativos['catetoA'],
+            40,
+            (Math.PI/180)*180, 
+            (Math.PI/180)*(180+valoresAbsolutos['anguloA']), 
+            false); //desenha o ângulo do ponto B
 
         
         
@@ -82,18 +96,20 @@ export function Triangulo({canvasSize, valoresAbsolutos, canvasPadding}:Triangul
         
         ctx.fillStyle = "white"
         ctx.font = "20px Arial"
-        ctx.fillText("A", pontoPartida['x']-18,250);
-        ctx.fillText("a", pontoPartida['x']-10,pontoPartida['y']-5);
-        ctx.fillText("b", pontoPartida['x']+valoresRelativos['catetoB']+5,pontoPartida['y']+valoresRelativos['catetoA']+5);
-        ctx.fillText("B", 250, pontoPartida['y']+valoresRelativos['catetoA']+20);
 
-        ctx.fillText("C", pontoPartida['x']+0.5*valoresRelativos['catetoB'], pontoPartida['y']+(0.45)*valoresRelativos['catetoA']);
-        ctx.fillText("H1", pontoPartida['x']+0.25*valoresRelativos['catetoB'], pontoPartida['y']+(0.22)*valoresRelativos['catetoA']);
-        ctx.fillText("H2", pontoPartida['x']+0.80*valoresRelativos['catetoB'], pontoPartida['y']+(0.75)*valoresRelativos['catetoA']);
-        ctx.fillText("H", pontoPartida['x']+(proporcaoHipotenusa/2*valoresRelativos['catetoB']), pontoPartida['y']+(proporcaoHipotenusa*1.45)*valoresRelativos['catetoA']);
+        // legendas
+        ctx.fillText("a", pontoPartida['x']-18,250);
+        ctx.fillText("A", pontoPartida['x']-10,pontoPartida['y']-5);
+        ctx.fillText("B", pontoPartida['x']+valoresRelativos['catetoB']+5,pontoPartida['y']+valoresRelativos['catetoA']+5);
+        ctx.fillText("b", 250, pontoPartida['y']+valoresRelativos['catetoA']+20);
+
+        ctx.fillText("c", pontoPartida['x']+0.5*valoresRelativos['catetoB'], pontoPartida['y']+(0.45)*valoresRelativos['catetoA']);
+        ctx.fillText("m", pontoPartida['x']+0.25*valoresRelativos['catetoB'], pontoPartida['y']+(0.22)*valoresRelativos['catetoA']);
+        ctx.fillText("n", pontoPartida['x']+0.80*valoresRelativos['catetoB'], pontoPartida['y']+(0.75)*valoresRelativos['catetoA']);
+        ctx.fillText("h", pontoPartida['x']+(proporcaoHipotenusa/2*valoresRelativos['catetoB']), pontoPartida['y']+(proporcaoHipotenusa*1.45)*valoresRelativos['catetoA']);
 
         
-        ctx.fillRect(pontoPartida['x'], pontoPartida['y']+valoresRelativos['catetoA']-40, 40, 40);
+        ctx.fillRect(pontoPartida['x'], pontoPartida['y']+valoresRelativos['catetoA']-40, 40, 40); // quadrado do ângulo reto
         ctx.fill();
     }}/>
 }
