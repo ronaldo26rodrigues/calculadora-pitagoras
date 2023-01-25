137eaed651a5afa1760c0c85aa7aeed42b9a987e
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2023-01-25 16:53:51 -0300

    refatoração no backend

--- server/main.py
+++ server/main.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
-from math import sqrt, asin, atan, tan, sin, degrees, radians
+from math import sqrt, atan, tan, sin
+from util import *
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/calculadora")
-def hello():
-    global triangulo
+def calculadora():
     triangulo = {
         'catetoA': float(request.args.get('catetoA')),
         'catetoB': float(request.args.get('catetoB')),
@@ -19,7 +20,7 @@ def hello():
         'h2': float(request.args.get('h2')),
         'area': float(request.args.get('area'))
     }
-    deg2rad()
+    deg2rad(triangulo)
     if triangulo['catetoA'] != 0 and triangulo['catetoB'] != 0 and triangulo['hipotenusa'] != 0:
         if e_retangulo(triangulo['catetoA'], triangulo['catetoB'], triangulo['hipotenusa']):
             pass
@@ -28,15 +29,19 @@ def hello():
     try:
         if triangulo['catetoA'] != 0 and triangulo['catetoB'] != 0:
             triangulo['hipotenusa'] = sqrt(triangulo['catetoA']**2+triangulo['catetoB']**2)
-            calcular_angulo_ab()
+            calcular_angulo_ab(triangulo)
         elif triangulo['catetoA'] != 0 and triangulo['hipotenusa'] != 0:
             triangulo['catetoB'] = sqrt(triangulo['hipotenusa']**2-triangulo['catetoA']**2)
-            calcular_angulo_ab()
+            calcular_angulo_ab(triangulo)
         elif triangulo['catetoB'] != 0 and triangulo['hipotenusa'] != 0:
             triangulo['catetoA'] = sqrt(triangulo['hipotenusa']**2-triangulo['catetoB']**2)
-            calcular_angulo_ab()
+            calcular_angulo_ab(triangulo)
+    except ZeroDivisionError:
+        return 'Houve uma divisão por zero', 400
 
-        if (triangulo['anguloA'] != 0 and triangulo['anguloB'] != 0) and (triangulo['catetoA'] == 0 or triangulo['catetoB'] == 0 or triangulo['hipotenusa'] == 0):
+    try:
+        if (triangulo['anguloA'] != 0 and triangulo['anguloB'] != 0) and \
+           (triangulo['catetoA'] == 0 or triangulo['catetoB'] == 0 or triangulo['hipotenusa'] == 0):
             if triangulo['catetoA'] != 0:
                 triangulo['catetoB'] = triangulo['catetoA']/atan(triangulo['anguloB'])
                 triangulo['hipotenusa'] = sqrt(triangulo['catetoA'] ** 2 + triangulo['catetoB'] ** 2)
@@ -46,43 +51,16 @@ def hello():
             if triangulo['hipotenusa'] != 0:
                 triangulo['catetoA'] = sin(triangulo['anguloA'])*triangulo['hipotenusa']
                 triangulo['catetoB'] = sqrt(triangulo['hipotenusa'] ** 2 - triangulo['catetoA'] ** 2)
+
     except Exception:
         return 'Estes valores podem não estar corretos', 400
-
 
     triangulo['altura'] = (triangulo['catetoA']*triangulo['catetoB'])/triangulo['hipotenusa']
     triangulo['h1'] = sqrt(triangulo['catetoA']**2-triangulo['altura']**2)
     triangulo['h2'] = triangulo['hipotenusa'] - triangulo['h1']
     triangulo['area'] = (triangulo['catetoB']*triangulo['catetoA'])/2
-    rad2deg()
+    rad2deg(triangulo)
 
-    print(triangulo)
     return triangulo, 200
 
 
-def e_retangulo(a, b, c):
-    return (a*a+b*b == c*c) or (c*c+b*b == a*a) or (a*a+c*c == b*b)
-
-
-def calcular_angulo_a():
-    print(triangulo['catetoA'])
-    triangulo['anguloA'] = asin(triangulo['catetoA']/triangulo['hipotenusa'])
-
-
-def calcular_angulo_b():
-    triangulo['anguloB'] = asin(triangulo['catetoB']/triangulo['hipotenusa'])
-
-
-def calcular_angulo_ab():
-    calcular_angulo_a()
-    calcular_angulo_b()
-
-
-def rad2deg():
-    triangulo['anguloA'] = degrees(triangulo['anguloA'])
-    triangulo['anguloB'] = degrees(triangulo['anguloB'])
-
-
-def deg2rad():
-    triangulo['anguloA'] = radians(triangulo['anguloA'])
-    triangulo['anguloB'] = radians(triangulo['anguloB'])
